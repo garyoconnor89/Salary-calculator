069c19cabeeb38aaaf30a43b0c912ac34e80d383
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 13:18:19 +0100

    add components to App file

--- salary-calculator/src/App.js
+++ salary-calculator/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import SalaryInput from "./components/SalaryInput";
+import SalaryDisplay from "./components/SalaryDisplay";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <SalaryInput />
+      <SalaryDisplay />
     </div>
   );
 }
