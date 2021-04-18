cdc0d062bb14875761a8991151b7bf523d0a64b1
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 13:19:47 +0100

    add basic Header with placeholders and no styling

---
+++ salary-calculator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Pay Calculator</h1>
+      <h3>Describe basic function</h3>
+    </div>
+  );
+};
+
+export default Header;
