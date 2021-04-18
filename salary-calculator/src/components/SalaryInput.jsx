f7416d1543d6325124cae915d0f527edfdd95d94
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 13:20:18 +0100

    add basic input form with no styling

---
+++ salary-calculator/src/components/SalaryInput.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SalaryInput = () => {
+  return (
+    <div>
+      <form>
+        <label>Input Salary:</label>
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SalaryInput;
