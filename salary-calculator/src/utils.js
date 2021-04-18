62b7513eb44557eab14d9f3ee721f57beb4b562e
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 12:27:28 +0100

    add functionality to payCalculator for salaries between 15000 and 50000

--- salary-calculator/src/utils.js
+++ salary-calculator/src/utils.js
@@ -1,11 +1,31 @@
 function payCalculator(salary) {
+  if (salary < 15000) {
+    return {
+      pay: salary,
+      tax: 0,
+      NI: 0,
+      payPercentage: 100,
+      taxPercentage: 0,
+      NIPercentage: 0,
+    };
+  }
+
+  const midBracket = salary - 15000;
+  const tax = Math.round((midBracket / 100) * 20);
+  const NI = Math.round((midBracket / 100) * 12);
+  const pay = salary - tax - NI;
+
+  const payPercentage = Math.round((pay / salary) * 100);
+  const taxPercentage = Math.round((tax / salary) * 100);
+  const NIPercentage = Math.round((NI / salary) * 100);
+
   return {
-    pay: salary,
-    tax: 0,
-    NI: 0,
-    payPercentage: 100,
-    taxPercentage: 0,
-    NIPercentage: 0,
+    pay,
+    tax,
+    NI,
+    payPercentage,
+    taxPercentage,
+    NIPercentage,
   };
 }
 
