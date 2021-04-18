8a9ec5a1ed792e7d0df8899ee1a065473071ddff
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 12:54:42 +0100

    add completed functionality for payCalculator

--- salary-calculator/src/utils.js
+++ salary-calculator/src/utils.js
@@ -1,32 +1,40 @@
 function payCalculator(salary) {
+  let pay = 0;
+  let tax = 0;
+  let NI = 0;
+  let payPercentage = 0;
+  let taxPercentage = 0;
+  let NIPercentage = 0;
+
   if (salary < 15000) {
     return {
       pay: salary,
-      tax: 0,
-      NI: 0,
+      tax,
+      NI,
       payPercentage: 100,
-      taxPercentage: 0,
-      NIPercentage: 0,
+      taxPercentage,
+      NIPercentage,
     };
   }
 
-  const midBracket = salary - 15000;
-  const tax = Math.round((midBracket / 100) * 20);
-  const NI = Math.round((midBracket / 100) * 12);
-  const pay = salary - tax - NI;
+  if (salary < 50000) {
+    const midBracket = salary - 15000;
+    tax = Math.round((midBracket / 100) * 20);
+    NI = Math.round((midBracket / 100) * 12);
+  } else {
+    const topBracket = salary - 50000;
+    const topBracketTax = (topBracket / 100) * 40;
+    const topBracketNI = (topBracket / 100) * 2;
 
-  const payPercentage = Math.round((pay / salary) * 100);
-  const taxPercentage = Math.round((tax / salary) * 100);
-  const NIPercentage = Math.round((NI / salary) * 100);
+    tax = Math.round(topBracketTax + 7000);
+    NI = Math.round(topBracketNI + 4200);
+  }
+  pay = salary - tax - NI;
+  payPercentage = Math.round((pay / salary) * 100);
+  taxPercentage = Math.round((tax / salary) * 100);
+  NIPercentage = Math.round((NI / salary) * 100);
 
-  return {
-    pay,
-    tax,
-    NI,
-    payPercentage,
-    taxPercentage,
-    NIPercentage,
-  };
+  return { pay, tax, NI, payPercentage, taxPercentage, NIPercentage };
 }
 
 module.exports = payCalculator;
