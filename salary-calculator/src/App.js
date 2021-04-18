41b674a0afe0fb66366638022fb99fae6760a96b
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 15:35:27 +0100

    alter paths on App

--- salary-calculator/src/App.js
+++ salary-calculator/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
 import SalaryInput from "./components/SalaryInput";
-import SalaryDisplay from "./components/SalaryDisplay";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SalaryInput />
-      <SalaryDisplay />
     </div>
   );
 }
