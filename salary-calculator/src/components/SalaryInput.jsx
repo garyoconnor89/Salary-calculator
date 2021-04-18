a369c7ed5f88b627b88ddc60dcdb178924f45baf
Gary OConnor <garyoconnor50@gmail.com>
2021-04-18 15:36:36 +0100

    add functions on change and submit, render SalaryDisplay component on submit

--- salary-calculator/src/components/SalaryInput.jsx
+++ salary-calculator/src/components/SalaryInput.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import SalaryDisplay from "./SalaryDisplay";
 
-const SalaryInput = () => {
-  return (
-    <div>
-      <form>
-        <label>Input Salary:</label>
-        <input type="text" />
-        <input type="submit" />
-      </form>
-    </div>
-  );
-};
+class SalaryInput extends Component {
+  state = {
+    salary_value: "",
+    hasSubmitted: false,
+  };
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({ salary_value: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ hasSubmitted: true });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.hasSubmitted ? (
+          <h1>Here's your salary!</h1>
+        ) : (
+          <form onSubmit={this.handleSubmit}>
+            // <label>Input Salary:</label>
+            //{" "}
+            <input
+              type="text"
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
+            // <button>Enter!</button>
+            //
+          </form>
+        )}
+
+        {this.state.hasSubmitted ? (
+          <SalaryDisplay salary_value={this.state.salary_value} />
+        ) : (
+          <h1>false</h1>
+        )}
+      </div>
+    );
+  }
+}
 
 export default SalaryInput;
